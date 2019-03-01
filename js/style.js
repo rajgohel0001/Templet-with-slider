91e522dbacdd8696dd786d04a5e06b9c99bdc43d
rajgohel0001 <rajgohel855@gmail.com>
2019-03-01 19:21:05 +0530

    PSD-with-slider

---
+++ js/style.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.slide').slick({
+		dots:true,
+		arrows:true,
+		autoplay:true,
+		autoplaySpeed:2000,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" area-hedden="true"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" area-hedden="true"></i></button>'
+	});
+});
